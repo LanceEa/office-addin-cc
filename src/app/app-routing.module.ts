c10069f715668e1976a86547812aa8f1ad89e1b0
Lance Austin <lance.austin@brambles.com>
2021-05-07 11:30:27 -0500

    update deps to latest angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,8 +23,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true
-  })],
+    useHash: true,
+    relativeLinkResolution: 'legacy'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
